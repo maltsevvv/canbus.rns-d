2c4513d55d9e16ae8b3169f1b3be4be504e3438d
maltsevvv <maltsev.v.v@hotmail.com>
2020-04-27 01:06:51 +0300

    Delete can0.py

--- can0.py
+++
@@ -1,3 +0,0 @@
-import os
-#os.system("sudo ip link set can0 up type can bitrate 100000 restart-ms 100 loopback on") (FOR TESTING ONLY)
-os.system("ip link set can0 up type can bitrate 100000 restart-ms 100")
